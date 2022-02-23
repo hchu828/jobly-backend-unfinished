5d8ebfdbc6d64bc9c9238646aa928cedfcf3e02b
Sam Courtenay <sam.m.courtenay@gmail.com>
2022-02-23 10:22:01 -0800

    documented partial sql function. Working on testing

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,16 +1,27 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Accepts an object as the first argument which will be used to populate the 
+ * column headers in a SQL statement. Accepts a JS object in the second argument
+ * to be converted to SQL field values corresponding to the column headers
+ * 
+ * Returns an object with 2 keys. setCols for column headers to be SET in the UPDATE
+ * command. Values to be provided to the placeholders in the SQL statement
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
+
+
+  // This takes our array of keys and maps it so that the key becomes the
+  // column name, and the value placeholder ($1) is the index of the key +1.
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
+  // Returns the template / string to run in SQL
   return {
     setCols: cols.join(", "),
     values: Object.values(dataToUpdate),
