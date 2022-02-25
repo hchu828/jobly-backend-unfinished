8c8767968d12717250225758d88724de2a1e94c5
hectorchu~ <hector.chu828@gmail.com>
2022-02-25 12:22:01 -0800

    deployment changes

--- config.js
+++ config.js
@@ -7,13 +7,14 @@ require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const PORT = +process.env.PORT || 3001;
+// const PORT = +process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+    ? "jobly_test"
+    : process.env.DATABASE_URL || "jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
--- db.js
+++ db.js
@@ -5,8 +5,13 @@
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
+const DB_URI = (process.env.NODE_ENV === "test")
+  ? "jobly_test"
+  : process.env.DATABASE_URL || "jobly";
+
 const db = new Client({
-  connectionString: getDatabaseUri(),
+  // connectionString: getDatabaseUri(),
+  connectionString: DB_URI,
 });
 
 db.connect();
